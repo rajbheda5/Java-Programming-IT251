5350e08309b556e2110642dcad8ff0857013e4f1
Raj Bheda <rajbheda5@gmail.com>
2020-07-28 13:44:55 +0530

    Update sort.java

--- sort.java
+++ sort.java
@@ -4,14 +4,56 @@ public class sort {
     public static void main(String[] args){
         //numeric array sorting
         Integer[] numArray = {10, 50, 40, 20, 60, 30, 80, 100, 90, 70};
-        System.out.printf("Original Numeric Array: %s", Arrays.toString(numArray));
-        Arrays.sort(numArray);
-        System.out.printf("\nSorted Numeric Array: %s\n\n", Arrays.toString(numArray));
-        
+        System.out.printf("Original Numeric Array: ");
+        for(int i=0; i<numArray.length; i++){
+            if(i == numArray.length -1 )
+                System.out.printf(numArray[i]+"");
+            else
+                System.out.printf(numArray[i] + ", ");
+        }
+        for (int i = 0; i < numArray.length; i++){
+            for (int j = i+1; j < numArray.length; j++){
+                int tmp = 0;
+                if(numArray[i]>numArray[j]){
+                    tmp = numArray[i];
+                    numArray[i] = numArray[j];
+                    numArray[j] = tmp;
+                }
+            }
+        }
+        System.out.printf("\nSorted Numeric Array: ");
+        for(int i=0; i<numArray.length; i++){
+            if(i == numArray.length -1 )
+                System.out.printf(numArray[i]+"\n\n");
+            else
+                System.out.printf(numArray[i] + ", ");
+        }
+
         //string array sorting
         String[] strArray = {"Elephant", "Cat", "Lion", "Dog", "Tiger"};
-        System.out.printf("Original String Array: %s", Arrays.toString(strArray));
-        Arrays.sort(strArray);
-        System.out.printf("\nSorted String Array: %s", Arrays.toString(strArray));
+        System.out.printf("Original String Array: ");
+        for(int i=0; i<strArray.length; i++){
+            if(i == strArray.length -1 )
+                System.out.printf(strArray[i]+"");
+            else
+                System.out.printf(strArray[i] + ", ");
+        }
+        for(int i = 0; i < strArray.length ; i++){
+            for(int j = i+1; j < strArray.length ; j++){
+                if(strArray[i].compareTo(strArray[j]) > 0){
+                    String tmp = strArray[i];
+                    strArray[i] = strArray[j];
+                    strArray[j] = tmp;
+                }
+            }
+        }
+
+        System.out.printf("\nSorted String Array: ");
+        for(int i=0; i<strArray.length; i++){
+            if(i == strArray.length -1 )
+                System.out.printf(strArray[i]+"");
+            else
+                System.out.printf(strArray[i] + ", ");
+        }
     }
 }
