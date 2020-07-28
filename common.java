e340d21ccb270705b0f3f161c298a00a437e6dac
Raj Bheda <rajbheda5@gmail.com>
2020-07-28 15:52:33 +0530

    Update common.java

--- common.java
+++ common.java
@@ -2,8 +2,8 @@ import java.util.Arrays;
 
 public class common {
     public static void main(String[] args){
-        int[] array1 = {2, 4, 6, 8, 10, 12};
-        int[] array2 = {3, 6, 9, 12, 15, 18};
+        String[] array1 = {"Java", "CPP", "CCP", "Kotlin", "Swift"};
+        String[] array2 = {"JavaScript", "Java", "Dart", "Python"};
         System.out.printf("Array 1: " + Arrays.toString(array1));
         System.out.printf("\nArray 2: " + Arrays.toString(array2));
 
@@ -11,7 +11,7 @@ public class common {
 
         for (int i = 0; i < array1.length; i++){
             for(int j = 0; j< array2.length; j++){
-                if(array1[i] == array2[j]){
+                if(array1[i].compareTo(array2[j]) == 0){
                     System.out.println(array1[i]);
                     break;
                 }
